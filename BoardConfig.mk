80d668e56169e94c6d83a9447748738eefa33abd
Yiğit Emre Yanık <60074077+yanikyigitt@users.noreply.github.com>
2020-07-06 03:00:37 +0300

    GM8_sprout : bringup Q #2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,9 +44,13 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_BOOTLOADER_BOARD_NAME := msm8937
 TARGET_NO_BOOTLOADER := true
 
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_PHONY_TARGETS := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -98,6 +102,10 @@ endif
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := (1 << 21)
 TARGET_USES_HWC2 := true
+
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
+PRODUCT_USES_QCOM_HARDWARE := true
 
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
